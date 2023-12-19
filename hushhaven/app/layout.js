f2896ec16cfcb2e6f5ae292c8fa022b6d382a62f
Hwan Eung Song <coryhsong@gmail.com>
2023-12-19 15:38:49 -0500

    cookie banner issue

--- hushhaven/app/layout.js
+++ hushhaven/app/layout.js
@@ -1,7 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import GoogleAnalytics from '../app/components/GoogleAnalytics';
-import CookieBanner from './components/cookieBanner';
 
 const inter = Inter({ subsets: ['latin'] })
 
