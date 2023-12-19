9b8fbf23ebd96c3079e46ceab962ec96f77dc8e8
hwaneungsong <90282169+hwaneungsong@users.noreply.github.com>
2023-12-19 16:18:56 -0500

    Update route.js

--- hushhaven/app/api/newsletter/route.js
+++ hushhaven/app/api/newsletter/route.js
@@ -1,28 +1,47 @@
-import { NextResponse } from "next/server";
+import axios from 'axios';
 
-export async function POST(req, res) {
-  const { email } = await req.json();
-  if (!email) {
-    return res.status(400).json({ error: "Email is required" });
+export default async (req, res) => {
+  if (req.method !== 'POST') {
+    res.setHeader('Allow', ['POST']);
+    return res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 
-  const MailchimpKey = process.env.MAILCHIMP_API_KEY;
-  const MailchimpServer = process.env.MAILCHIMP_API_SERVER;
-  const MailchimpAudience = process.env.MAILCHIMP_AUDIENCE_ID;
+  const { email } = req.body;
+  if (!email || !email.includes('@')) {
+    return res.status(400).json({ error: 'Email is required and should be valid.' });
+  }
 
-  const customUrl = `https://${MailchimpServer}.api.mailchimp.com/lists/${MailchimpAudience}/members`;
+  const MAILCHIMP_API_KEY = process.env.MAILCHIMP_API_KEY;
+  const MAILCHIMP_AUDIENCE_ID = process.env.MAILCHIMP_AUDIENCE_ID;
 
-  const response = await fetch(customUrl, {
-    method: "POST",
-    headers: {
-      Authorization: `apikey ${MailchimpKey}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      email_address: email,
-      status: "subscribed",
-    }),
-  });
-  const received = await response.json();
-  return NextResponse.json(received);
-}
+  if (!MAILCHIMP_API_KEY || !MAILCHIMP_AUDIENCE_ID) {
+    return res.status(500).json({ error: 'Server is not configured for Mailchimp.' });
+  }
+
+  const dataCenter = MAILCHIMP_API_KEY.split('-')[1];
+  const url = `https://${dataCenter}.api.mailchimp.com/3.0/lists/${MAILCHIMP_AUDIENCE_ID}/members`;
+
+  try {
+    const response = await axios.post(
+      url,
+      {
+        email_address: email,
+        status: 'subscribed', // You might want to use 'pending' to send a confirmation email
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `apikey ${MAILCHIMP_API_KEY}`
+        }
+      }
+    );
+
+    if (response.status >= 400) {
+      return res.status(400).json({ error: `There was an error subscribing to the newsletter. Send this info to the server admin: ${response.status}` });
+    }
+
+    return res.status(200).json({ message: 'Success! 🎉 You are now subscribed to the newsletter.' });
+  } catch (error) {
+    return res.status(500).json({ error: error.response?.data || 'There was an error subscribing to the newsletter.' });
+  }
+};
